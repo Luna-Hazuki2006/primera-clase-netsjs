43e19bf95ed36823a78b0a18c0997b004b17f173
Luna Hazuki <lunahazuki2006@outlook.com>
2025-07-30 11:46:33 -0400

    Con la pseudoestructura para que podamos trabajar

--- src/categorias/categorias.service.ts
+++ src/categorias/categorias.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class CategoriasService {}
+export class CategoriasService {
+    verCategoria() {
+        return 
+    }
+
+    listarCategorias() {
+        return
+    }
+
+    guardarCategorias() {
+        return
+    }
+}
