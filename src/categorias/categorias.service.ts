02a57bbb70bf083c7ee64128373504ec8159f6b2
Siberiaaa <siberiamvg@gmail.com>
2025-07-30 11:54:49 -0400

    categorias

--- src/categorias/categorias.service.ts
+++ src/categorias/categorias.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class CategoriasService {
+  private categorias = ['categoria 1', 'categoria 2', 'categoria 3', 'categoria 4',];
+
     verCategoria() {
         return 
     }
