b86e9d77dd0abf0f9e9e9c4bd72f90aa14610788
Luna Hazuki <lunahazuki2006@outlook.com>
2025-07-30 12:08:13 -0400

    Un commit con ver categoría con excepciones y listas

--- src/categorias/categorias.service.ts
+++ src/categorias/categorias.service.ts
@@ -1,15 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class CategoriasService {
-  private categorias = ['categoria 1', 'categoria 2', 'categoria 3', 'categoria 4',];
+    private categorias = ['categoria 1', 'categoria 2', 'categoria 3', 'categoria 4',];
 
-    verCategoria() {
-        return this.categorias;
+    verCategoria(id : number) {
+        if (id % 1 != 0) throw new BadRequestException()
+        if (id > this.categorias.length - 1 || id < 0) throw new NotFoundException()
+        return this.categorias[id];
     }
 
     listarCategorias() {
-        return
+        return this.categorias
     }
 
     guardarCategorias(data: string) {
