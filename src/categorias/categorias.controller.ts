811ed63c85ec302102e3a45b7c3f43aa554f071a
Luna Hazuki <lunahazuki2006@outlook.com>
2025-07-30 12:12:45 -0400

    Técnicamente listo

--- src/categorias/categorias.controller.ts
+++ src/categorias/categorias.controller.ts
@@ -1,15 +1,21 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CategoriasService } from './categorias.service';
 
 @Controller('categorias')
 export class CategoriasController {
-  constructor(private readonly categoriasService: CategoriasService) {}
-  @Get('all')
-  getCategorias(): Array<string> {
-    return this.categoriasService.verCategoria();
-  }
-  @Post('create')
-  createProduct(@Body() data: { name: string }) {
-    return this.categoriasService.guardarCategorias(data.name);
-  }
+    constructor(private readonly categoriasService: CategoriasService) {}
+    @Get('all')
+    getCategorias(): Array<string> {
+        return this.categoriasService.listarCategorias();
+    }
+
+    @Get(':id') 
+    getCategoria(@Param('id') id : number) {
+        return this.categoriasService.verCategoria(id)
+    }
+
+    @Post('create')
+    createProduct(@Body() data: { name: string }) {
+        return this.categoriasService.guardarCategorias(data.name);
+    }
 }
