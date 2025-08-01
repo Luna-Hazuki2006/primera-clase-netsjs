ef867364c7b45d18cb9753e366a67f1feb0a3a0e
Luna Hazuki <lunahazuki2006@outlook.com>
2025-08-01 09:29:22 -0400

    fe de erratas

--- src/usuarios/usuarios.controller.ts
+++ src/usuarios/usuarios.controller.ts
@@ -6,7 +6,7 @@ export class UsuariosController {
     constructor(private readonly usuarios: UsuariosController) {}
 
     @Post('/register')
-    mostrarProductos(@Body() datos : Creacion, @Res() respuesta : Response) {
+    registrar(@Body() datos : Creacion, @Res() respuesta : Response) {
         // return respuesta
     }
 }
